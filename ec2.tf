69a0d91ae6b214c00d389b38aa796214d4b43dd4
George Onalo <onalotech7@gmail.com>
2023-04-02 14:54:33 +0100

    update

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraform-user"
+# profile = "George"
 }
 
 
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
+    bucket  = "george-terraform-remote-state"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
-    profile = "terraform-user"
+  # profile = "George"
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "my2ndkey"
   user_data              = file("install_techmax.sh")
 
   tags = {
